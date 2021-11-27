6aed6d50f4390e40c92ed9eed9f272a239b67290
Kyungseo-Park <troublesome.dev@gmail.com>
2021-11-27 17:10:18 +0900

    fix controller => restControlelr

--- demo/src/main/java/com/example/demo/SampleController.java
+++ demo/src/main/java/com/example/demo/SampleController.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +8,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.Setter;
 import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RestController;
 
 @Log
-@Controller
+@RestController
 public class SampleController {
 
     @Setter(onMethod_ = @Autowired)
     private KakaoPay kakaoPay;
+
+    @GetMapping("/")
+    public String hmoe(){
+        log.info("WQEQE WWWt~~~~~~~~~~~~~~~");
+
+        return "/kakaoPayss";
+    }
 
     @GetMapping("/kakaoPay")
     public String kakaoPayGet(){
